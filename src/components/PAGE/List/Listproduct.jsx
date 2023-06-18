b3d02903b2011ad2080cb0225baa996f7f74837f
jhonitay <jhonitay06@gmail.com>
2023-06-18 10:49:27 +0700

    fitur: order & logo favicon

---
+++ src/components/PAGE/List/Listproduct.jsx
@@ -0,0 +1,15 @@
+import './Listproduct.css'
+
+function Listproduct(){
+    return(
+        <>
+        <div className='list'>
+            <div className='icon'>Gambar</div>
+            <div className='nama-produk'>Nama</div>
+            <div className='jumlah'>Jumlah</div>
+            <div className='harga'>harga satuan</div>
+        </div>
+        </>
+    )
+}
+export default Listproduct;

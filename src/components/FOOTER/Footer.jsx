034c1865466af2d5d7c538aa8407afad5a26ad46
jhonitay <jhonitay06@gmail.com>
2023-06-19 17:44:55 +0700

    memperbaiki css yng bertabrakan

--- src/components/FOOTER/Footer.jsx
+++ src/components/FOOTER/Footer.jsx
@@ -1,36 +1,31 @@
-import './Footer.css';
+import "./Footer.css";
 
-function Footer(){
-    return (
-        <>
-        <div className='footer'>
-            <div>
-                <div className='phone'>
-                    <p>
-                       <b>Phone:</b> +628756372 
-                    </p>
-                    <p>
-                    <b>ADDRESS :</b> Candi Gebang
-                    </p>
-                </div>
-                
-
-                <div className='icon'>
-                    <div className='coffie'>
-                    coffiee
-                    </div>
-                    <div className='sosmed'>
-                   <img src="" alt="img" />
-                   <img src="" alt="img" />
-                   <img src="" alt="img" />
-                   <img src="" alt="img" />
-                    </div>
-                </div>
-                
-                
-            </div>
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <div>
+          <div className="phone">
+            <p>
+              <b>Phone:</b> +628756372
+            </p>
+            <p>
+              <b>ADDRESS :</b> Candi Gebang
+            </p>
+          </div>
         </div>
-        </>
-    )
+
+          <div className="ion">
+            <div className="coffie">coffiee</div>
+            <div className="sosmed">
+              <img src="" alt="img" />
+              <img src="" alt="img" />
+              <img src="" alt="img" />
+              <img src="" alt="img" />
+            </div>
+          </div>
+      </div>
+    </>
+  );
 }
 export default Footer;

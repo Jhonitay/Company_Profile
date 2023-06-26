e016ef437342ce588fa03be52a7960e4d4ba534c
AdityaAnanta123 <Kadek3912@students.amikom.ac.id>
2023-06-26 19:20:20 +0700

    Perbaiki server

--- server/server.js
+++ server/server.js
@@ -15,12 +15,3 @@ app.use(Express.json);
 app.use(router);
 
 app.listen(5000, ()=> console.log('Server running at'));
-
-const app = express();
-
-app.use("/", (req, res, next) => {
-    res.send("Hello World");
-});
-app.listen(4000, () => {
-    console.log('Server telah ada');
-});

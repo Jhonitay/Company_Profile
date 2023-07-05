fe7f03464e41539d41af12df7fb118b685bb8207
jhonitay <jhonitay06@gmail.com>
2023-07-05 17:11:51 +0700

    merger

--- src/components/NAVBAR/NavbarAdmin.jsx
+++ src/components/NAVBAR/NavbarAdmin.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 function NavbarAdmin(){
     return(
         <>
-        div>h1
+        <div>hhh</div>
         </>
     )
 }
--- src/pages/PageLogin.jsx
+++ src/pages/PageLogin.jsx
@@ -1,22 +1,17 @@
 // import { Route } from "react-router-dom";
 import Login from "../components/LOGIN/Login";
+import Navbar from "../components/NAVBAR/Navbar";
+import Footer from "../components/FOOTER/Footer";
 // import SignUp from "../components/SignUp/SignUp";
-import React from "react";
 // import { Link, Route } from "react-router-dom";
-
-
 
 function PageLogin() {
   return (
-    <div className="page">
-        {/* <SignUp/> */}
-        <Login/>
-
-        
-        
-
-    </div>
+    <>
+        <Navbar />
+        <Login />
+        <Footer />
+    </>
   );
 }
-
 export default PageLogin;

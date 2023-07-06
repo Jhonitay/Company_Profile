671389d5c030541e31bc45ecfc11e576a02549e1
jhonitay <jhonitay06@gmail.com>
2023-07-06 23:42:04 +0700

    memanggil API untul Login dn  SignUp

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express =  require("express");
 const { router } =  require("./routes/index.js");
-
+const cors = require("cors");
 const port = 5000;
-
 const app = express();
 
 // try {
@@ -11,18 +10,22 @@ const app = express();
 // } catch (error) {
 //     console.error(error);
 // }
-
+app.use(cors({
+  origin : "http://localhost:3000",
+  credentials : true
+}));
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Hello World! Hello");
-  console.log("tes")
-});
 app.use(router);
-app.get("/about", (req, res) => {
-  res.send("Hello World! Hello");
-  console.log("tes")
-});
-
 app.listen(port, () => {
   console.log(port);
 });
+// app.get("/", (req, res) => {
+//   res.send("Hello World! Hello");
+//   console.log("tes")
+// });
+// app.get("/about", (req, res) => {
+//   res.send("Hello World! Hello");
+//   console.log("tes")
+// });
+
+

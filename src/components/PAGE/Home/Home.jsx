48251f935bdc512aa728e7033b594cb0b9b48c48
ahmdfauzzan <ahmadfauzan.280.af@gmail.com>
2023-07-07 13:25:35 +0700

    perbarui home

--- src/components/PAGE/Home/Home.jsx
+++ src/components/PAGE/Home/Home.jsx
@@ -1,17 +1,17 @@
 import "./Home.css";
 import Slider from "../Slider/Slider";
-import { Link, useNavigate } from "react-router-dom";
+import Sliderimg from "../Sliderimg/Sliderimg";
+import { Link } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
   return (
     <>
       <div>
         <ul className="contain">
           <Slider />
 
-          <li class="content2">
-            {/* content 2 */}
+          {/* <li class="content2">
+           
             <div class="side">
               <div class="flex side-content2">
                 <li class="sideLeft2 align-center item-center">
@@ -58,15 +58,12 @@ function Home() {
               <div class="sideRight2 ">
                 <h3 class="test">Our coffee is hotter than Angelina Jolie.</h3>
                 <p>
-                  Selamat datang di kedai kopi kami, tempat di mana cinta dan
-                  hangatnya suasana bertemu dalam secangkir kopi. Kami bangga
-                  mempersembahkan pengalaman kopi yang tak tertandingi, yang tak
-                  hanya memenuhi kebutuhanmu akan rasa, tetapi juga menyentuh
-                  jiwa.
+                  Selamat datang di kedai kopi kami, tempat di mana cinta dan hangatnya suasana bertemu dalam secangkir kopi. Kami bangga mempersembahkan pengalaman kopi yang tak tertandingi, yang tak hanya memenuhi kebutuhanmu akan rasa,
+                  tetapi juga menyentuh jiwa.
                 </p>
               </div>
             </div>
-          </li>
+          </li> */}
 
           <li class="content3">
             {/* content 3 */}
@@ -74,11 +71,7 @@ function Home() {
               <div class="sideLeft3 ">
                 <h1 class="sideLeft3-title">TIME DISCOVER</h1>
                 <h1 class="sideLeft3-title">COFFE HOUSE</h1>
-                <p class="sideLeft3-text">
-                  Kami menawarkan beragam varietas kopi, mulai dari single
-                  origin hingga campuran unik, yang akan memenuhi keinginan dan
-                  preferensi penikmat kopi paling cerdas.
-                </p>
+                <p class="sideLeft3-text">Kami menawarkan beragam varietas kopi, mulai dari single origin hingga campuran unik, yang akan memenuhi keinginan dan preferensi penikmat kopi paling cerdas.</p>
                 <div class="flex ">
                   <button class="button-content3 black">
                     <Link to="/Order" className="Link">
@@ -104,21 +97,21 @@ function Home() {
             <h1 class="service align-center item-center">Our Service</h1>
             <div class="ourService ">
               <div class="serviceBox align-center item-center">
-                <div>
-                  <img src="" alt="IMG"></img>
-                  <h3>service 1</h3>
+                <div className="boxservice">
+                  <img src="/images/icon-aftersales.png" alt="IMG"></img>
+                  <h3>After Sale</h3>
                 </div>
               </div>
               <div class="serviceBox align-center item-center">
-                <div>
-                  <img src="" alt="IMG"></img>
-                  <h3>service 1</h3>
+                <div className="boxservice">
+                  <img src="/images/icon-beverage.png" alt="IMG"></img>
+                  <h3>Tools and Materials</h3>
                 </div>
               </div>
               <div class="serviceBox align-center item-center">
-                <div>
-                  <img src="" alt="IMG"></img>
-                  <h3>service 1</h3>
+                <div className="boxservice">
+                  <img src="/images/icon-businnes.png" alt="IMG"></img>
+                  <h3>Businnes Consultan</h3>
                 </div>
               </div>
             </div>
@@ -127,22 +120,16 @@ function Home() {
           <li class="content5">
             {/* content 5 */}
             <div class="imgContent5 align-center item-center">
-              <h1>IMG</h1>
+              <Sliderimg />
             </div>
-            <div class="descContent5 align-center item-center">
+            <div class="descContent5">
               <div class="box-text">
-                <h1 class="font-brown">new product</h1>
+                <h1 class="font-brown">New Product</h1>
                 <h1 class="size2">ORDER NOW</h1>
-                <p class="box-text5">
-                  Kami dengan bangga mempersembahkan layanan pemesanan kopi
-                  online yang mudah dan cepat, sehingga kamu dapat menikmati
-                  secangkir kopi pilihanmu kapan pun dan di mana pun.
-                </p>
-                <button class="button-content5">
-                  <Link to="/Order" className="Link">
-                    view all product
-                  </Link>
-                </button>
+                <p class="box-text5">Kami dengan bangga mempersembahkan layanan pemesanan kopi online yang mudah dan cepat, sehingga kamu dapat menikmati secangkir kopi pilihanmu kapan pun dan di mana pun.</p>
+                <Link to="/Order" class="button-content5">
+                  View all product
+                </Link>
               </div>
             </div>
           </li>
@@ -151,16 +138,11 @@ function Home() {
             {/* content 6 */}
             <div>
               <h1 class="question">Have a Question?</h1>
-              <p class="question">
-                Jika Anda memiliki pertanyaan untuk kami, silahkan menghubungi
-                team kami.
-              </p>
+              <p class="question">Jika Anda memiliki pertanyaan untuk kami, silahkan menghubungi team kami.</p>
               <div class="question">
-                <button class="buttonClickHere black">
-                  <Link to="/Contact" class="Link">
-                    click here
-                  </Link>
-                </button>
+                <Link to="/Contact" class="buttonClickHere black">
+                  Click Here
+                </Link>
               </div>
             </div>
           </li>
